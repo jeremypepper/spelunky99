9de79e7bd38dcbd5e160e6a112c6522cf2b2b725
Jeremy Hay <jeremy@jhay.net>
2021-05-31 06:43:31 -0700

    live score

---
+++ script/scoreboard.js
@@ -0,0 +1,51 @@
+const templatePlayerRow = _.template(
+    `
+        <tr>
+            <td class="playercell"><%- From %></td>
+            <td><%- World %>-<%- Sublevel %></td>
+            <td><%- HP %></td>
+            <td><%- ThreatLevel %></td>
+            <td><%- Kills %></td>
+            <td><%- Assists %></td>
+        </tr>
+    `
+);
+const templateTable = _.template(
+    `
+        <table>
+            <thead>
+                <tr>
+                    <th class="playercell">Player</th>
+                    <th>Level</th>
+                    <th>HP</th>
+                    <th>Traps</th>
+                    <th>Kills</th>
+                    <th>Assists</th>
+                </tr>
+            <thead>
+            <tbody>
+                <%= Body %>
+            </tbody>
+        </table>
+    `
+)
+function updateTable() {
+    fetch("https://w5addyotsd.execute-api.us-west-2.amazonaws.com/prod4")
+        .then(response => response.json())
+        .then(data => {
+            var currentPlayerCount = data.data.Messages.length
+            _.forEach(data.data.Messages, m=> m.ThreatLevel = Math.round(m.ThreatLevel))
+            var rows = _.map(data.data.Messages, m => templatePlayerRow(m))
+            var table = templateTable({Body:rows.join('\n')})
+            if (currentPlayerCount == 1) {
+                document.getElementById("playercount").innerText = currentPlayerCount + " player online"
+            }
+            else {
+                document.getElementById("playercount").innerText = currentPlayerCount + " players online"
+            }
+            
+            document.getElementById("currentplayers").innerHTML = table
+        })
+}
+updateTable()
+window.setInterval(updateTable, 10 * 1000);
